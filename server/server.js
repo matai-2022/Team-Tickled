c37ea2bb196220883edaf9ab1b8ef2ca35d0cd6f
Ciara <cduncumnz@gmail.com>
2022-07-01 11:18:37 +1200

    holy shit its posting

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const path = require('path')
 
 // const fruitRoutes = require('./routes/fruits')
+const itemsRoutes = require('./routes/items')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-// server.use('/api/v1/fruits', fruitRoutes)
+server.use('/', itemsRoutes)
 
 module.exports = server
